71658b46750bd237742a2b9473eb734f45324aaf
momo <ahdbsgh2@naver.com>
2021-03-09 14:41:51 +0900

    Study

--- Study/Data.py
+++ Study/Data.py
@@ -9,20 +9,20 @@ from nltk.tokenize import TreebankWordTokenizer
 from torchtext.legacy.data import TabularDataset
 from torchtext.legacy.data import Iterator
 
-# tokenize_ko = Mecab()
-# tokenize_en = TreebankWordTokenizer()
+token_ko = Mecab()
+token_en = TreebankWordTokenizer()
 
 def tokenize_ko(text):
     """
-    Tokenizes German text from a string into a list of strings (tokens) and reverses it
+    Tokenizes Korean text from a string into a list of strings (tokens) and reverses it
     """
-    return [tok.text for tok in Mecab(text)][::-1]
+    return [tok.text for tok in token_ko.morphs(text)][::-1]
 
 def tokenize_en(text):
     """
     Tokenizes English text from a string into a list of strings (tokens)
     """
-    return [tok.text for tok in TreebankWordTokenizer(text)]
+    return [tok.text for tok in token_en.tokenize(text)]
 
 Korpora.fetch('korean_parallel_koen_news')
 
@@ -37,12 +37,15 @@ en = corpus.train.pairs
 tok_ko = Field(tokenize = tokenize_ko,
             init_token = '<sos>',
             eos_token = '<eos>',
-            lower = True)
+            lower = False)
 
 tok_en = Field(tokenize = tokenize_en,
             init_token = '<sos>',
             eos_token = '<eos>',
             lower = True)
 
+# print(ko)
+
 print(tok_ko.tokenize(ko))
 print(tok_en.tokenize(en))
+
