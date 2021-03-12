8ed4556141ae274b5a87005904bef7eea5a72386
momo <ahdbsgh2@naver.com>
2021-03-12 14:43:53 +0900

    study

--- Study/GPT-2.py
+++ Study/GPT-2.py
@@ -1,11 +1,14 @@
 from transformers import GPT2Tokenizer, GPT2Model
+from transformers import pipeline, set_seed
+
+
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2Model.from_pretrained('gpt2')
 text = "Replace me by any text you'd like."
 encoded_input = tokenizer(text, return_tensors='pt')
 output = model(**encoded_input)
-
-from transformers import pipeline, set_seed
 generator = pipeline('text-generation', model='gpt2')
 set_seed(42)
-generator("Hi my name is", max_length=10, num_return_sequences=5)
+generation_text= generator("Hi my name is", max_length=10, num_return_sequences=5)
+
+print(generation_text)
