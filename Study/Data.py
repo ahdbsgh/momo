046cd18dc91350e397f1754aec5bd9963a6848b5
momo <ahdbsgh2@naver.com>
2021-03-15 00:00:41 +0900

    Study

--- Study/Data.py
+++ Study/Data.py
@@ -24,9 +24,9 @@ token_en = TreebankWordTokenizer()
 #     """
 #     return [tok.text for tok in token_en.tokenize(text)]
 
-Korpora.fetch('korean_parallel_koen_news')
-
-corpus = Korpora.load("korean_parallel_koen_news")
+# Korpora.fetch('korean_parallel_koen_news')
+#
+# corpus = Korpora.load("korean_parallel_koen_news")
 
 # print(corpus)
 # ko = corpus.train.texts
@@ -37,20 +37,20 @@ corpus = Korpora.load("korean_parallel_koen_news")
 
 
 
-kor = Field(tokenize = token_ko.morphs,
-            init_token = '<sos>',
-            eos_token = '<eos>',
-            lower = False)
-
-eng = Field(tokenize = token_en.tokenize,
-            init_token = '<sos>',
-            eos_token = '<eos>',
-            lower = True)
+# kor = Field(tokenize = token_ko.morphs,
+#             init_token = '<sos>',
+#             eos_token = '<eos>',
+#             lower = False)
 #
-# train_data, test_data = TabularDataset.splits(
-#         path='.', train=corpus.train, test=corpus.test, format='tsv',
-#         fields=[('kor', kor), ('eng', eng)])
-train_data, valid_data, test_data = corpus.splits(fields = (kor, eng))
+# eng = Field(tokenize = token_en.tokenize,
+#             init_token = '<sos>',
+#             eos_token = '<eos>',
+#             lower = True)
+# #
+# # train_data, test_data = TabularDataset.splits(
+# #         path='.', train=corpus.train, test=corpus.test, format='tsv',
+# #         fields=[('kor', kor), ('eng', eng)])
+# train_data, valid_data, test_data = corpus.splits(fields = (kor, eng))
 
 
 # print(ko)
@@ -62,6 +62,10 @@ train_data, valid_data, test_data = corpus.splits(fields = (kor, eng))
 #
 # print(token_ko.morphs(test))
 
+from hanspell import spell_checker
+result = spell_checker.check('안녕? 나는 모윤호라고해 나는 한국인이야. 밥은먹었니?.')
+result.as_dict()  # dict로 출력
+print(result)
 
 #todo: torchtext 로 사용하니 안된다. 다른 SOS, EOS를 넣는 방법을 생각해보자
 
